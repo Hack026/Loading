c5af59052f92f5fb35407b855240348a3d69a8ac
Padmanaava Das <124140193+Hack026@users.noreply.github.com>
2023-09-22 20:22:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const btn=document.querySelector('.button'),
+    spinIcon=document.querySelector(".spinner"),
+    btnText=document.querySelector(".btn-text");
+
+btn.addEventListener("click", ()=>{
+    btn.style.cursor = "wait";
+    btn.classList.add("checked");
+    spinIcon.classList.add("spin");
+    btnText.textContent="Loading";
+    
+    setTimeout(() => {
+        btn.style.pointerEvents = "none";
+        spinIcon.classList.replace("spinner" , "check");
+        spinIcon.classList.replace("fa-circle-notch" , "fa-check")
+        btnText.textContent="Done";
+    }, 5000);
+
+
+})
